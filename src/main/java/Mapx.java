d74c467cbfde6f74c3ea222bed951fea5fcb2778
Sehajpreet Singh <iamsherrysingh@gmail.com>
2019-09-29 21:19:07 -0400

    Started constructing gameGraph

--- src/main/java/Mapx.java
+++ src/main/java/Mapx.java
@@ -2,12 +2,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Mapx {
     private String continents, countries, borders;
     Mapx()
     {
-        continents=countries=borders="";
+        continents=countries= borders ="";
     }
     private void loadMap() throws FileNotFoundException {
 
@@ -33,7 +35,7 @@ public class Mapx {
 
             continents = sb.toString();
             continents=continents.trim();
-            System.out.println(continents);
+//            System.out.println(continents);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -65,7 +67,7 @@ public class Mapx {
 
             countries = sb.toString();
             countries=countries.trim();
-            System.out.println(countries);
+//            System.out.println(countries);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -94,8 +96,8 @@ public class Mapx {
             }
 
             borders = sb.toString();
-            borders=borders.trim();
-            System.out.println(borders);
+            borders = borders.trim();
+//            System.out.println(neighbours);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -103,7 +105,7 @@ public class Mapx {
         }
     }
 
-    public Graph GraphRepresentation()
+    public Graph graphRepresentation()
     {
         try
         {
@@ -113,10 +115,45 @@ public class Mapx {
         {
             System.out.println(f.getMessage());
         }
-//        Graph gameGraph=new Graph();
 
+        Graph gameGraph= new Graph(new ArrayList<Country>());
+
+        //Scan countries and add
+        //Create country object and borders from this.neighbours
+        Scanner countryScanner= new Scanner(this.countries);
+        countryScanner.nextLine(); //Ignoring first line of this.countries
+        while(countryScanner.hasNext()) {
+            String lineCountry= countryScanner.nextLine();
+            lineCountry= lineCountry.trim();
+            String countryLineSubstrings[]= lineCountry.split(" ");
+            System.out.println(countryLineSubstrings[0]+"*"+countryLineSubstrings[4]);
+            ArrayList<Integer> neighbours= new ArrayList<Integer>();
+            Scanner borderScanner= new Scanner(this.borders);
+            borderScanner.nextLine();
+            while(borderScanner.hasNext())
+            {
+                String lineBorder= borderScanner.nextLine();
+                lineBorder= lineBorder.trim();
+                String borderLineSubstrings[]= lineBorder.split(" ");
+                if(Integer.parseInt(borderLineSubstrings[0])==Integer.parseInt(countryLineSubstrings[0])){
+                    for(int i=1;i<borderLineSubstrings.length;i++){
+                        neighbours.add(Integer.parseInt(borderLineSubstrings[i]));
+                    }
+
+                }
+
+            }
+
+            Country country= new Country(Integer.parseInt(countryLineSubstrings[0]), countryLineSubstrings[1] ,Integer.parseInt(countryLineSubstrings[2]) ,null ,null ,Integer.parseInt(countryLineSubstrings[3]) ,Integer.parseInt(countryLineSubstrings[4]) ,neighbours);
+
+        }
 
         return null;
 
+    }
+
+    public static void main(String[] args) {
+        Mapx m= new Mapx();
+        m.graphRepresentation();
     }
 }
