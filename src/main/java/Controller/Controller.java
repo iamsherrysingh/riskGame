29cba782a832d932e64c2c1f2d2d821b55389d97
Mehrnoosh Amjadi <M_AMJA@encs.concordia.ca>
2019-10-05 04:34:33 -0400

    function getCommand() reimplemented

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -3,16 +3,17 @@ import Model.Mapx;
 import Model.Player;
 import java.io.IOException;
 import java.util.Scanner;
+import Controller.Controller.tasksEnum;
 import java.util.ArrayList;
-
+import java.util.*; 
 import Model.*;
 
 public class Controller {
 
 	enum States {mapEditor,gamePlay,startupPhase,editPlayer,troopArmies,reinforcementPhase,attackPhase,fortificationPhase }
-	enum Tasks {addcontinent,removecontinent,addcountry,removecountry,addneighbor,removeneighbor,savemap,editmap,validatemap,showmap,loadmap,addplayer,removeplayer,populatecountries}
+	enum tasksEnum {Unknown,addcontinent,removecontinent,addcountry,removecountry,addneighbor,removeneighbor,savemap,editmap,validatemap,showmap,loadmap,addplayer,removeplayer,populatecountries}
 	States currentState = States.mapEditor;
-	Tasks currentTask;
+	tasksEnum currentTask;
 	String continentName,countryName,neighborCountryName,mapFile,playerName;
 	Integer controlValue,playerId=1;
 	private Mapx mapObj;
@@ -20,135 +21,261 @@ public class Controller {
 	boolean editPlayerFinished = false;
 	
 	// parse input Instruction -> Command , Switch, Data
-	boolean getCommand(){
+	boolean getCommand(ArrayList<extractedTasks> tasksList){
+		
 		System.out.println("Enter Command");
 		Scanner scan = new Scanner(System.in);
 		String instruction = scan.nextLine().trim();
-		String[] command = instruction.split(" ");
-
-		if(command[0].equals("editcontinent")){
-			if(command.length == 4 && command[1].equals("-add")){
-				continentName = command[2];
-				controlValue = Integer.parseInt(command[3]);	
-				currentTask = Tasks.addcontinent;
-				return true;
-				// check control value number
-			}
-			else if(command.length == 3 && command[1].equals("-remove")){
-				continentName = command[2];
-				currentTask = Tasks.removecontinent;
-				return true;
-			}
-			else{
-				System.out.println("'wrong Command' ---> editcontinent -add continentname continentvalue");
-				System.out.println("                ---> editcontinent -remove continentname");
-				return false;
-			}
-		}
-		else if(command[0].equals("editcountry")){
-			if(command.length == 4 && command[1].equals("-add")){
-				countryName = command[2];
-				continentName = command[3];	
-				currentTask = Tasks.addcountry;
-				return true;
-			}
-			else if( command.length == 3 && command[1].equals("-remove")){
-				countryName = command[2];
-				currentTask = Tasks.removecountry;
-				return true;
-			}
-			else{
-				System.out.println("'wrong Command' ---> editcountry -add countryname continentname");
-				System.out.println("                ---> editcountry -remove countryname");
-				return false;
-			}	
-		}
-		else if(command[0].equals("editneighbor")){
-			if(command.length == 4 && command[1].equals("-add")){
-				countryName = command[2];
-				neighborCountryName = command[3];	
-				currentTask = Tasks.addneighbor;
-				return true;
-			}
-			else if(command.length == 4 && command[1].equals("-remove")){
-				countryName = command[2];
-				neighborCountryName = command[3];
-				currentTask = Tasks.removeneighbor;
-				return true;
-			}
-			else{
-				System.out.println("'wrong Command' ---> editneighbor -add countryname neighborcountryname");
-				System.out.println("                ---> editneighbor -remove countryname neighborcountryname");
-				return false;
-			}
-		}
-		else if(command[0].equals("savemap")){
-			if( command.length == 2 ) {
-				mapFile = command[1] + ".map";
-				currentTask = Tasks.savemap;
-				return true;
-			}
-			else {
-				System.out.println("wrong Command ---> savemap filename");
-				return false;
-			}
-		}
-		else if(command[0].equals("editmap")){
-			if( command.length == 2 ) {
-				mapFile = command[1] + ".map";
-				currentTask = Tasks.editmap;
-				return true;
-			}
-			else {
-				System.out.println("wrong Command ---> editmap filename");
-				return false;
-			}
-		}
-		else if(command[0].equals("validatemap")){
-			currentTask = Tasks.validatemap;
-			return true;
-		}
-		else if(command[0].equals("showmap")){
-			currentTask = Tasks.showmap;
-			return true;
-		}
-		else if(command[0].equals("loadmap")){
-			if( command.length == 2 ) {
-				mapFile = command[1] + ".map";
-				currentTask = Tasks.loadmap;
-				return true;
-			}
-			else {
-				System.out.println("wrong Command ---> loadmap filename");
-				return false;
-			}
-		}
-		else if(command[0].equals("gameplayer")){
-			if(command.length == 3 && command[1].equals("-add")) {
-				playerName = command[2];
-				currentTask = Tasks.addplayer;
-				return true;
-			}
-			else if(command.length == 3 && command[1].equals("-remove")){
-				playerName = command[2];
-				currentTask = Tasks.removeplayer;
-				return true;
-			}
-			else {
-				System.out.println("wrong Command ---> gameplayer -add playername");
-				System.out.println("                   gameplayer -remove playername");
-				return false;
-			}
-		}
-		else if(command[0].equals("populatecountries")){
-			currentTask = Tasks.populatecountries;
-			return true;
-		}
-		else {
+		List<String> command = Arrays.asList(instruction.split(" "));
+		ListIterator<String> cmdItr = command.listIterator();
+		
+		//get command part of instruction
+		String cmdStr;
+		if(!cmdItr.hasNext()) {
 			System.out.println("wrong Command");
 			return false;
 		}
+		cmdStr = cmdItr.next();	
+		
+		//command editcontinent
+		if(cmdStr.equals("editcontinent")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			while(cmdItr.hasNext())
+			{
+				//get and check the switches
+				cmdStr = cmdItr.next();
+				if(cmdStr.equals("-add")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.addcontinent;
+					
+					//get data related to addContinent task
+					for(int i=0;i<1;i++) {
+						if(!cmdItr.hasNext()) {
+							System.out.println("wrong Command");
+							return false;
+						}
+						eTask.taskData.add(cmdItr.next());
+					}
+					tasksList.add(eTask);
+				}
+				else if(cmdStr.equals("-remove")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.removecontinent;
+					
+					//get data related to removeContinent task
+					if(!cmdItr.hasNext()) {
+						System.out.println("wrong Command");
+						return false;
+					}
+					eTask.taskData.add(cmdItr.next());
+					tasksList.add(eTask);
+				}
+				else {
+					System.out.println("wrong Command");
+					return false;
+				}
+			}			
+		}
+		//command editcountry
+		else if(cmdStr.equals("editcountry")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			while(cmdItr.hasNext())
+			{
+				//get and check the switches
+				cmdStr = cmdItr.next();
+				if(cmdStr.equals("-add")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.addcountry;
+					
+					//get data related to addCountry task
+					for(int i=0;i<1;i++) {
+						if(!cmdItr.hasNext()) {
+							System.out.println("wrong Command");
+							return false;
+						}
+						eTask.taskData.add(cmdItr.next());
+					}
+					tasksList.add(eTask);
+				}
+				else if(cmdStr.equals("-remove")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.removecountry;
+					
+					//get data related to removeCountry task
+					if(!cmdItr.hasNext()) {
+						System.out.println("wrong Command");
+						return false;
+					}
+					eTask.taskData.add(cmdItr.next());
+					tasksList.add(eTask);
+				}
+				else {
+					System.out.println("wrong Command");
+					return false;
+				}
+			}			
+		}
+		//command editneighbor
+		else if(cmdStr.equals("editneighbor")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			while(cmdItr.hasNext())
+			{
+				//get and check the switches
+				cmdStr = cmdItr.next();
+				if(cmdStr.equals("-add")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.addneighbor;
+					
+					//get data related to addneighbor task
+					for(int i=0;i<1;i++) {
+						if(!cmdItr.hasNext()) {
+							System.out.println("wrong Command");
+							return false;
+						}
+						eTask.taskData.add(cmdItr.next());
+					}
+					tasksList.add(eTask);
+				}
+				else if(cmdStr.equals("-remove")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.removeneighbor;
+					
+					//get data related to removeneighbor task
+					for(int i=0;i<1;i++) {
+						if(!cmdItr.hasNext()) {
+							System.out.println("wrong Command");
+							return false;
+						}
+						eTask.taskData.add(cmdItr.next());
+					}
+					tasksList.add(eTask);
+				}
+				else {
+					System.out.println("wrong Command");
+					return false;
+				}
+			}			
+		}
+		//Command savemap
+		else if(cmdStr.equals("savemap")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			extractedTasks eTask = new extractedTasks();
+			eTask.name = tasksEnum.savemap;
+			
+			//get data related to savemap task
+			eTask.taskData.add(cmdItr.next());
+			
+			tasksList.add(eTask);
+		}
+		//Command editmap
+		else if(cmdStr.equals("editmap")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			extractedTasks eTask = new extractedTasks();
+			eTask.name = tasksEnum.editmap;
+			
+			//get data related to editmap task
+			eTask.taskData.add(cmdItr.next());
+			
+			tasksList.add(eTask);
+		}
+		//Command validatemap
+		else if(cmdStr.equals("validatemap")){
+			extractedTasks eTask = new extractedTasks();
+			eTask.name = tasksEnum.validatemap;
+			tasksList.add(eTask);
+		}
+		//Command showmap
+		else if(cmdStr.equals("showmap")){
+			extractedTasks eTask = new extractedTasks();
+			eTask.name = tasksEnum.showmap;
+			tasksList.add(eTask);
+		}
+		//Command loadmap
+		else if(cmdStr.equals("loadmap")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			extractedTasks eTask = new extractedTasks();
+			eTask.name = tasksEnum.loadmap;
+			
+			//get data related to loadmap task
+			eTask.taskData.add(cmdItr.next());
+			
+			tasksList.add(eTask);
+		}
+		//Command gameplayer
+		else if(cmdStr.equals("gameplayer")){
+			if(!cmdItr.hasNext()) {
+				System.out.println("wrong Command");
+				return false;
+			}
+			
+			while(cmdItr.hasNext())
+			{
+				//get and check the switches
+				cmdStr = cmdItr.next();
+				if(cmdStr.equals("-add")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.addplayer;
+					
+					//get data related to addPlayer task
+					if(!cmdItr.hasNext()) {
+						System.out.println("wrong Command");
+						return false;
+					}
+					eTask.taskData.add(cmdItr.next());
+					tasksList.add(eTask);
+				}
+				else if(cmdStr.equals("-remove")) {
+					extractedTasks eTask = new extractedTasks();
+					eTask.name = tasksEnum.removeplayer;
+					
+					//get data related to removePlayer task
+					if(!cmdItr.hasNext()) {
+						System.out.println("wrong Command");
+						return false;
+					}
+					eTask.taskData.add(cmdItr.next());
+					tasksList.add(eTask);
+				}
+				else {
+					System.out.println("wrong Command");
+					return false;
+				}
+			}			
+		}
+		//Command populatecountries
+		else if(cmdStr.equals("populatecountries")){
+			extractedTasks eTask = new extractedTasks();
+			eTask.name = tasksEnum.populatecountries;
+			tasksList.add(eTask);
+		}
+		
+		return true;
 	}
+		
     public static void main(String[] args) throws IOException {
 		try {
 			Controller controller = new Controller();
@@ -243,7 +370,11 @@ public class Controller {
 					        } 
 							break;
 						}		
-						default: System.out.println("Invalid Command. Please Enter Startup Phase Command");
+						default: {
+							System.out.println("Invalid Command. Please Enter Startup Phase Command");
+							controller.getCommand();
+						}
+						
 					}
 				}
 			}
@@ -269,4 +400,12 @@ public class Controller {
 //        Integer numberOfPlayers= Integer.parseInt(commandScanner.nextLine().trim());
        
     }   
+}
+class extractedTasks{
+	public tasksEnum name;
+	public ArrayList<String> taskData;
+	
+	public extractedTasks(){
+		taskData = new ArrayList<String>();
+	}
 }
