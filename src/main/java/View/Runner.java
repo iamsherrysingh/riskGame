c8e932181ea45105f2657d88613d446ccfca983d
Sehaj <iamsherrysingh@gmail.com>
2019-11-04 18:57:33 -0500

    added provision to add observer in Controller

---
+++ src/main/java/View/Runner.java
@@ -0,0 +1,9 @@
+package View;
+
+public class Runner {
+    public static void main(String[] args) {
+        PhaseView phaseView= new PhaseView();
+        System.out.println(phaseView.header);
+        System.out.println(phaseView.footer);
+    }
+}
