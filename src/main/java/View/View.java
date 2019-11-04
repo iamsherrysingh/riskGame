0b4390baffa0c44a95ff8e780435209226190326
Sehaj <iamsherrysingh@gmail.com>
2019-11-03 20:31:13 -0500

    Added 2 Views Added plantuml files to better understand the project

---
+++ src/main/java/View/View.java
@@ -0,0 +1,15 @@
+package View;
+
+public class View implements IObserver{
+
+    String headerElement1= "____________________";
+    String headerElement2= "|    Phase View    |";
+    String header= headerElement1 +"\n"+ headerElement2;
+
+
+
+    @Override
+    public void update() {
+
+    }
+}
