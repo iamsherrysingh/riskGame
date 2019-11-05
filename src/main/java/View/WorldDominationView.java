be2f34b743276276bf8616527b2cc10749de1c6f
Mehrnoosh Amjadi <M_AMJA@encs.concordia.ca>
2019-11-05 10:18:30 -0500

    conflict fixed

--- src/main/java/View/WorldDominationView.java
+++ src/main/java/View/WorldDominationView.java
@@ -12,7 +12,7 @@ public class WorldDominationView implements IObserver{
 
     String data="";
     @Override
-    public void update(GamePlay gamePlay) {
+    public void update(GamePlay gamePlay, Player playerobj) {
 
         try {
             data= "Map Ownership:";
