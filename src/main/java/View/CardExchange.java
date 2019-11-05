f2e1e54543e0c6a35b81f8e38959837236a1266d
Mehrnoosh Amjadi <M_AMJA@encs.concordia.ca>
2019-11-04 22:25:38 -0500

    minor changes

--- src/main/java/View/CardExchange.java
+++ src/main/java/View/CardExchange.java
@@ -3,6 +3,10 @@ import Model.GamePlay;
 
 public class CardExchange implements IObserver {
 	
+    String header=          "______________________________________________"  + "\n" +
+            				"_______________Card Exchange View_____________";
+    String footer=          "______________________________________________";
+    
 	@Override
 	public void update(GamePlay gameplay) {
 		System.out.println("These are your cards");
