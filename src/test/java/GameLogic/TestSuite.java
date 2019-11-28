e4a9704d31cc951470e03232d4b72fb758efe92e
Sehajpreet Singh <iamsherrysingh@gmail.com>
2019-11-27 19:41:47 -0500

    minor change

--- src/test/java/GameLogic/TestSuite.java
+++ src/test/java/GameLogic/TestSuite.java
@@ -4,11 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 /**
  * This is a Junit testsuite that is used to run the tests defined in:
- * {@link GameLogic.ContinentTest}, {@link GameLogic.CountryTest}, {@link GameLogic.PlayerTest}, {@link GameLogic.MapxTest}, {@link GameLogic.GamePlayTest}
+ * {@link GameLogic.ContinentTest}, {@link GameLogic.CountryTest},  {@link GameLogic.MapxTest}, {@link GameLogic.GamePlayTest}
  * 
  * @author birjotsingh17
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ContinentTest.class, CountryTest.class, PlayerTest.class, MapxTest.class, GamePlayTest.class})
+@Suite.SuiteClasses({ContinentTest.class, CountryTest.class, MapxTest.class, GamePlayTest.class})
 public class TestSuite {
 }
