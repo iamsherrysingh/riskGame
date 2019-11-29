841e772c66f6127472b6053529f292c5f0a3672d
Sehajpreet Singh <iamsherrysingh@gmail.com>
2019-11-28 19:28:35 -0500

    Added some methods

--- src/main/java/GameLogic/AggressivePlayer.java
+++ src/main/java/GameLogic/AggressivePlayer.java
@@ -95,19 +95,9 @@ public class AggressivePlayer extends Player implements IPlayer {
 
     }
 
-    @Override
-    /**
-     * This method attacks until no attack is possible using maximum number of dice to attack or defend
-     * @param fromCountry It is a String and denotes the name of the country from where the armies are to be sent
-     * @param toCountry It is a String and denotes the name of the country that is to be attacked
-     * @param graphObj It is an object of the class Graph
-     * @param currentPlayerObj It is an object of the class CurrentPlayer
-     * @return true(if the method executes successfully) or false(if the country entered is invalid or some other validation fails)
-     */
-    public boolean attackAllout(String fromCountry, String toCountry, Graph graphObj, CurrentPlayer currentPlayerObj) {
-
+    public Country getStrongestCountry(CurrentPlayer currentPlayerObj){
         Country strongestCountry=null;
-        for (Country country : graphObj.getAdjList()) {
+        for (Country country : GamePlay.getInstance().getGraphObj().getAdjList()) {
 
             if (country.getOwner().equalsIgnoreCase(currentPlayerObj.getCurrentPlayer().getName())) {
                 if (strongestCountry == null) {
@@ -120,169 +110,92 @@ public class AggressivePlayer extends Player implements IPlayer {
                 }
             }
         }
-        Country attackerCountry = strongestCountry;
-        System.out.println(GamePlay.getInstance().getCurrentPlayerName()+" attacking from "+strongestCountry.getName());
-        Country defenderCountry = null;
-        Integer weakestCountryNumber = null;
 
-        if (attackerCountry.neighbours.size() != 0) {
+        return strongestCountry;
+    }
 
-/*            for (int i = 0; i < attackerCountry.neighbours.size(); i++) {
-
-                Integer countryNumber = attackerCountry.neighbours.get(i);
-
-                if (weakestCountryNumber == null) {
-                    weakestCountryNumber = countryNumber;
-                } else if (Country.getCountryByNumber(weakestCountryNumber, graphObj).getNumberOfArmies() > Country.getCountryByNumber(countryNumber, graphObj).getNumberOfArmies() &&
-                        Country.getCountryByNumber(weakestCountryNumber, graphObj).getOwner()!=Country.getCountryByNumber(countryNumber, graphObj).getOwner() ){
-                    weakestCountryNumber = countryNumber;
-                }
-
-            }*/
-
-            ArrayList<Integer> nlist= attackerCountry.neighbours;
-
-            for(Integer nnumbr:nlist) {
-                Country neighbour= Country.getCountryByNumber(nnumbr, GamePlay.getInstance().getGraphObj());
-                if(!(neighbour.getOwner().equalsIgnoreCase(attackerCountry.getOwner())))
-                    continue;
-                if (defenderCountry == null) {
-                    defenderCountry = neighbour;
-                    // countryName=country.getName();
-                } else if(neighbour.getNumberOfArmies() < defenderCountry.getNumberOfArmies()) {
-                    defenderCountry=neighbour;
-                }
+    public Country getWeakestNeighbourEnemy(Country attackerCountry){
+        ArrayList<Integer> nlist= attackerCountry.neighbours;
+        Country weakestNeighbourEnemy=null;
+        for(Integer nnumbr:nlist) {
+            Country neighbour= Country.getCountryByNumber(nnumbr, GamePlay.getInstance().getGraphObj());
+            if(!(neighbour.getOwner().equalsIgnoreCase(attackerCountry.getOwner())))
+                continue;
+            if (weakestNeighbourEnemy == null) {
+                weakestNeighbourEnemy = neighbour;
+                // countryName=country.getName();
+            } else if(neighbour.getNumberOfArmies() < weakestNeighbourEnemy.getNumberOfArmies()) {
+                weakestNeighbourEnemy=neighbour;
             }
-            if(defenderCountry == null){ //All neighbours conquered
-                System.out.println(attackerCountry.owner+" owns all countries around"+attackerCountry.getName());
-                return true;
-//                attackAllout("","",graphObj,currentPlayerObj);
-            }
-
-
-
-            //defenderCountry = Country.getCountryByNumber(weakestCountryNumber, graphObj);
-            System.out.println("Attacking "+defenderCountry.getName());
-            String attackerName = attackerCountry.getOwner();
-            String defenderName = defenderCountry.getOwner();
-            IPlayer attacker = Database.getPlayerByName(attackerName);
-            IPlayer defender = Database.getPlayerByName(defenderName);
-            System.out.println("Defender Country player "+defenderCountry.getOwner()
-            +"\n"+ "attacker country player"+attackerCountry.getOwner());
-            if (attackerCountry.getOwner().equalsIgnoreCase(currentPlayerObj.getCurrentPlayer().getName())) {
-
-                if (defenderCountry.getOwner().equalsIgnoreCase(currentPlayerObj.getCurrentPlayer().getName())) {
-
-                    System.out.println("You can only attack the countries that are owned by some other player");
-                    return false;
-                } else {
-
-                    if (attackerCountry.neighbours.contains(defenderCountry.getNumber())) {
-
-                        Integer AttackerArmiesSelected = null;
-                        Integer DefenderArmiesSelected = null;
-
-                        if (attackerCountry.getNumberOfArmies() > 3) {
-                            // armies selected
-                            AttackerArmiesSelected = 3;
-                        } else if (attackerCountry.getNumberOfArmies() == 3) {
-                            // armies selected 2
-                            AttackerArmiesSelected = 2;
-                        } else if (attackerCountry.getNumberOfArmies() == 2) {
-                            // armies selected 1
-                            AttackerArmiesSelected = 1;
-                        } else {
-                            System.out.println("Cannot attack anymore " + fromCountry + ", has only 1 army left!");
-                        }
-
-                        if (defenderCountry.getNumberOfArmies() >= 2) {
-                            // armies selected 2
-                            DefenderArmiesSelected = 2;
-                        } else if (defenderCountry.getNumberOfArmies() == 1) {
-                            // armies selected 1
-                            DefenderArmiesSelected = 1;
-                        } else {
-                            System.out.println("No more armies to defend the country");
-                        }
-
-                        System.out.println("Before Atack");
-                        System.out.println("Attacker Armies : " + Country
-                                .getCountryByName(attackerCountry.name, Graph.getInstance()).getNumberOfArmies());
-                        System.out.println("Defender Armies : " + Country
-                                .getCountryByName(defenderCountry.name, Graph.getInstance()).getNumberOfArmies());
-                        battle(attackerCountry, defenderCountry, AttackerArmiesSelected, DefenderArmiesSelected,
-                                attacker, defender);
-                        AttackerArmiesSelected = null;
-                        DefenderArmiesSelected = null;
-                        System.out.println("After Atack");
-                        System.out.println("Attacker Armies : " + Country
-                                .getCountryByName(attackerCountry.name, Graph.getInstance()).getNumberOfArmies());
-                        System.out.println("Defender Armies : " + Country
-                                .getCountryByName(defenderCountry.name, Graph.getInstance()).getNumberOfArmies());
-                        System.out.println("--------------------------------");
-
-                        if (defenderCountry.getNumberOfArmies() == 0) {
-                            System.out.println("Attacker won the country " + defenderCountry.name);
-                            attacker.setMyCountries(defenderCountry.getNumber());
-                            defenderCountry.setOwner(attackerCountry.getOwner());
-                            defender.getMyCountries().remove(defenderCountry.getNumber());
-                            setCountryConquered(true);
-                            if (defender.getMyCountries().size() == 0) {
-                                defenderRemoved = true;
-                            }
-                            // System.out.println("Please enter a command to move armies to " +
-                            // defenderCountry.name);
-                            // System.out.println("Please select a number greater than or equal to " +
-                            // lastDiceSelected
-                            // + " and less than " + attackerCountry.getNumberOfArmies());
-
-                            Integer attackMove = null;
-
-                            attackMove = lastDiceSelected;
-
-                            if (attackMove != null) {
-                                attackMove(attackerCountry, defenderCountry, attackMove);
-                            } else {
-                                System.out.println("something went wrong!!");
-                            }
-                            System.out.println("AttackAllOut is finished here.");
-
-                        } else if (attackerCountry.getNumberOfArmies() == 1) {
-                            System.out.println("no attack anymore possible!!!");
-                        } else {
-                            attackAllout(fromCountry, toCountry, graphObj, currentPlayerObj);
-                        }
-
-                    } else {
-                        System.out.println("Attacker country and the defender country should be adjacent");
-                        return false;
-                    }
-                }
-            } else {
-                System.out.println("Please select the country owned by you("
-                        + currentPlayerObj.getCurrentPlayer().getName() + ") as attackerCountry");
-                return false;
-            }
-
-            GamePlay.getInstance().setCurrentOperation(
-                    "Performing all-out attack from " + attackerCountry.name + " to " + defenderCountry.name);
-
-            // if defender lost all of his country, attacker will owned all of his cards.
-            if ( getDefenderRemoved() == true) {
-
-                for (Card itr : defender.getPlayerCards()) {
-                    Card tempcard = itr;
-                    tempcard.setOwner(currentPlayerObj.getCurrentPlayer().getNumber());
-                    currentPlayerObj.getCurrentPlayer().setPlayerCards(tempcard);
-                }
-                Database.removePlayer(defenderName);
-                setDefenderRemoved(false);
-            }
-            return true;
-        } else {
-            System.out.println("All the neighbouring countries are owned by the current player");
-            return false;
         }
+        return weakestNeighbourEnemy;
+    }
+    public boolean attackAllout(String fromCountry, String toCountry, Graph graphObj, CurrentPlayer currentPlayerObj) {
+        Country strongestCountry=getStrongestCountry(currentPlayerObj);
+        Country weakestNeighbourEnemy= getWeakestNeighbourEnemy(strongestCountry);
+        if(weakestNeighbourEnemy==null){//All neighbours conquered
+                                        //fortify armies to strongest neighbour with 1 or more enemy
+            fortifyStrongestNeighbour(strongestCountry);
+            return true;
+        }
+        System.out.println(">>>>"+strongestCountry.name+" attacking "+weakestNeighbourEnemy.name);
+
+        while (getWeakestNeighbourEnemy(strongestCountry) != null) {
+            Integer AttackerArmiesSelected = null;
+            Integer DefenderArmiesSelected = null;
+            if (strongestCountry.getNumberOfArmies() > 3) {
+                // armies selected
+                AttackerArmiesSelected = 3;
+            } else if (strongestCountry.getNumberOfArmies() == 3) {
+                // armies selected 2
+                AttackerArmiesSelected = 2;
+            } else if (strongestCountry.getNumberOfArmies() == 2) {
+                // armies selected 1
+                AttackerArmiesSelected = 1;
+            } else{
+                //Fortify Strongest country with max armies possible
+                return true;
+            }
+
+            if (weakestNeighbourEnemy.getNumberOfArmies() >= 2) {
+                // armies selected 2
+                DefenderArmiesSelected = 2;
+            } else if (weakestNeighbourEnemy.getNumberOfArmies() < 2) {
+                // armies selected 1
+                DefenderArmiesSelected = 1;
+            }
+
+            battle(strongestCountry, weakestNeighbourEnemy, AttackerArmiesSelected, DefenderArmiesSelected,
+                    Database.getPlayerByName(strongestCountry.owner), Database.getPlayerByName(weakestNeighbourEnemy.owner));
+            System.out.println("battle Executed");
+            Scanner sc=new Scanner((System.in));
+            sc.nextLine();
+            if (weakestNeighbourEnemy.getNumberOfArmies() == 0) {
+                System.out.println("Attacker won the country " + weakestNeighbourEnemy.name);
+                Database.getPlayerByName(strongestCountry.owner).setMyCountries(weakestNeighbourEnemy.getNumber());
+                weakestNeighbourEnemy.setOwner(strongestCountry.getOwner());
+                Database.getPlayerByName(weakestNeighbourEnemy.owner).getMyCountries().remove(weakestNeighbourEnemy.getNumber());
+                setCountryConquered(true);
+                if (Database.getPlayerByName(weakestNeighbourEnemy.owner).getMyCountries().size() == 0) {
+                    for (Card itr : Database.getPlayerByName(weakestNeighbourEnemy.owner).getPlayerCards()) {
+                        Card tempcard = itr;
+                        tempcard.setOwner(currentPlayerObj.getCurrentPlayer().getNumber());
+                        currentPlayerObj.getCurrentPlayer().setPlayerCards(tempcard);
+                    }
+                    Database.getInstance().getPlayerList().remove(Database.getPlayerByName(weakestNeighbourEnemy.owner).getNumber() - 1);
+                    defenderRemoved = false;
+                }
+            }
+                //attack move
+                weakestNeighbourEnemy.setNumberOfArmies(weakestNeighbourEnemy.getNumberOfArmies()+AttackerArmiesSelected);
+                strongestCountry.setNumberOfArmies(strongestCountry.getNumberOfArmies()-AttackerArmiesSelected);
+
+
+        }
+        return true;
+    }
+
+    private void fortifyStrongestNeighbour(Country strongestCountry) {
 
     }
 
@@ -291,17 +204,17 @@ public class AggressivePlayer extends Player implements IPlayer {
      * This method defines how the attack takes place.
      * It covers all the steps of the aggressive attack mode.
      * @param attackerCountry It is an object of the class Country and gives the attacking country
-     * @param defenderCountry It is an object of the class Country and gives the defending country
+     * @param weakestNeighbourEnemy It is an object of the class Country and gives the defending country
      * @param numberOfArmiesToMove It is an integer value of the total number of armies that attack the defending country
      * @return true(If the method executes and the the attack move executes successfully) or false(If the number of armies entered is invalid or any other validation fails)
      */
-    public static boolean attackMove(Country attackerCountry, Country defenderCountry, Integer numberOfArmiesToMove) {
+    public static boolean attackMove(Country attackerCountry, Country weakestNeighbourEnemy, Integer numberOfArmiesToMove) {
 
-        if (defenderCountry.getNumberOfArmies() == 0) {
+        if (weakestNeighbourEnemy.getNumberOfArmies() == 0) {
 
             if (attackerCountry.getNumberOfArmies() > numberOfArmiesToMove) {
 
-                defenderCountry.setNumberOfArmies(numberOfArmiesToMove);
+                weakestNeighbourEnemy.setNumberOfArmies(numberOfArmiesToMove);
                 attackerCountry.setNumberOfArmies(attackerCountry.getNumberOfArmies() - numberOfArmiesToMove);
 
             } else {
@@ -330,16 +243,16 @@ public class AggressivePlayer extends Player implements IPlayer {
 
 
     /**
-     * This method takes care of the battle between the attackerCountry and the defenderCountry and is responsible for declaring whoever wins the battle
+     * This method takes care of the battle between the attackerCountry and the weakestNeighbourEnemy and is responsible for declaring whoever wins the battle
      * @param attackerCountry object of the country that attacks another country
-     * @param defenderCountry object of the country that defends itself in the turn
+     * @param weakestNeighbourEnemy object of the country that defends itself in the turn
      * @param attackerArmies Integer number of armies that are to be used for attack
      * @param defenderArmies Integer number of armies that are to be used for defend
      * @param attacker Object of the class IPlayer
      * @param defender Object of the class IPlayer
      * @return true(If the method executes and battle is executed successfully)
      */
-    public static boolean battle(Country attackerCountry, Country defenderCountry, Integer attackerArmies,
+    public static boolean battle(Country attackerCountry, Country weakestNeighbourEnemy, Integer attackerArmies,
                                  Integer defenderArmies, IPlayer attacker, IPlayer defender) {
 
         lastDiceSelected = attackerArmies;
@@ -391,7 +304,7 @@ public class AggressivePlayer extends Player implements IPlayer {
         }
 
         attackerCountry.setNumberOfArmies(attackerCountry.getNumberOfArmies() - attackerArmiesKilled);
-        defenderCountry.setNumberOfArmies(defenderCountry.getNumberOfArmies() - defenderArmiesKilled);
+        weakestNeighbourEnemy.setNumberOfArmies(weakestNeighbourEnemy.getNumberOfArmies() - defenderArmiesKilled);
 
         return true;
     }
@@ -448,7 +361,7 @@ public class AggressivePlayer extends Player implements IPlayer {
             System.out.println("A player has to own both the countries");
             return false;
         } else if (!(Mapx.checkPath(toCountry.name,fromCountry.name, gameGraph))) {
-            System.out.println("There should be path between two countries to fortify");
+            System.out.println("weakestNeighbourEnemyd be path between two countries to fortify");
             return false;
         } else if ((fromCountry.getNumberOfArmies() - numberOfArmies < 1)) {
             System.out.println("You must leave at least 1 army unit behind");
